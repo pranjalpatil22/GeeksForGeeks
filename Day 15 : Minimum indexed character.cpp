0bceefa59fc5dc436a7426c1552f051a650cde7d
pranjalpatil22 <141659726+pranjalpatil22@users.noreply.github.com>
2024-06-24 19:08:02 +0530

    Create Day 15 : Minimum indexed character.cpp

---
+++ Day 15 : Minimum indexed character.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to find the minimum indexed character.
+    int minIndexChar(string str, string patt)
+    {
+        // Your code here
+       unordered_set<char> s1(patt.begin() , patt.end());
+       
+       for(int i = 0; i < str.length(); i++)
+       {
+           if(s1.find(str[i]) != s1.end())
+           return i;
+       }
+       return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+    cin>>t;
+    while(t--)
+    {
+        string str;
+        string patt;
+        cin>>str;
+        cin>>patt;
+        Solution obj;
+        cout<<obj.minIndexChar(str, patt)<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
